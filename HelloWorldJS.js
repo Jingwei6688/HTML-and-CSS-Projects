824f48efeeb3ba5fb8154de190868e1268f7091a
Jingwei <ljingwei88@gmail.com>
2026-06-08 12:41:33 -0400

    modified 2 HelloWorld files

--- HelloWorldJS.js
+++ HelloWorldJS.js
@@ -1,1 +1,6 @@
-alert('Hello World, I am an alert box')
+//alert('Hello World, I am an alert box')
+function clickme() {
+  var str1 = "Hello World!"
+  var str2 = str1.fontcolor('green');
+  document.getElementById('ptag').innerHTML = str2;
+}
